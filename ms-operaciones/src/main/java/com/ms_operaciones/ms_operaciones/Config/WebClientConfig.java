d35c73dec57bbd16984e06977c426a424e68e61f
darien <dari.lizama@duocuc.cl>
2026-07-13 00:34:39 -0400

    Estructura final: Corrección de rutas de Wallet y desanidado de microservicios

--- ms-operaciones/src/main/java/com/ms_operaciones/ms_operaciones/Config/WebClientConfig.java
+++ ms-operaciones/src/main/java/com/ms_operaciones/ms_operaciones/Config/WebClientConfig.java
@@ -1,5 +1,13 @@
 package com.ms_operaciones.ms_operaciones.Config;
 
-public class WebClientConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 }
